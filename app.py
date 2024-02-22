01a8c57ac1b41b4a678cd5029a4a407c18a4d336
JustinKeeJoonHong <105581488+JustinKeeJoonHong@users.noreply.github.com>
2024-02-22 01:32:01 -0800

    add checking token header method

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 
 app = Flask(__name__)
 
+def get_token_auth_header():
+    if 'Authorization' not in request.headers:
+        abort(401)
+
+    auth_header =request.headers['Authorization']
+    header_parts = auth_header.split(' ')
+
+    if len(header_parts) != 2:
+        abort(401)
+    elif header_parts[0].lower() != 'bearer':
+        abort(401)
+
+    return header_parts[1]
+
 @app.route('/headers')
 def headers():
-    auth_header =request.headers['Authorization']
+    
+    jwt = get_token_auth_header()
 
-    header_parts = auth_header.split(' ')[1]
-    print(header_parts)
+    print(jwt)
     return 'not implemented'
