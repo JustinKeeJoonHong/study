971a541b09269a31d459f79e9ef1af941bf5cad8
JustinKeeJoonHong <105581488+JustinKeeJoonHong@users.noreply.github.com>
2024-04-23 19:03:39 -0700

    add subsribe method

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 function createStore() {
   let state;
+  let listeners = [];
 
   const getState = () => state;
 
+  const subsribe = (listener) => listeners.push(listener);
+
   return {
     getState,
+    subsribe,
   };
 }
